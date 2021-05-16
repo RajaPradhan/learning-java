cf82a00224b24af45ffcf159f9b43cae85c6a1e6
RajaPradhan <rajababu.pradhan@akelius.de>
2021-05-16 05:14:15 +0200

    first commit

---
+++ src/com/learningjava/Basics.java
@@ -0,0 +1,9 @@
+package com.learningjava;
+
+public class Basics {
+    public static void main(String[] args) {
+        String message = "hello world";
+
+        System.out.println(message);
+    }
+}
